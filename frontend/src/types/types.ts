4f390cd577bc9bfb6667936bc8434c84581f0dd7
repiyann <muhrepiyan26@gmail.com>
2024-12-06 14:31:49 +0700

    feat: new types for ActiveTab, LastPrice, and CustomAxisTickProps

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -2,3 +2,19 @@ export interface StockPrice {
 	date: string
 	close: number
 }
+
+export type ActiveTab = 'weekly' | 'monthly' | 'history'
+
+export interface LastPrice {
+	isPriceUp: boolean | null
+	lastPrice: number | null
+	percentage: number | null
+	prevPrice: number | null
+	priceChange: number | null
+}
+
+export interface CustomizedAxisTickProps {
+  x: number
+  y: number
+  payload: { value: string | number }
+}
